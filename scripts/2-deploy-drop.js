90219357608a3961ba8ab24591c19a1adaeddcbd
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-04-09 19:23:49 +0700

    :zap: deploy edition drop

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,35 @@
+import { AddressZero } from '@ethersproject/constants';
+import sdk from './1-initialize-sdk.js';
+import { readFileSync } from 'fs';
+
+(async () => {
+  try {
+    const editionDropAddress = await sdk.deployer.deployEditionDrop({
+      // The collection's name
+      name: 'Ultima MyDAO Pass',
+      // The collection's description
+      description: 'A DAO for self-govern individuals',
+      // The image that will be held on our NFT!
+      image: readFileSync('scripts/assets/edition-drop.png'),
+      // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the contract.
+      // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
+      // you can set this to your own wallet address if you want to charge for the drop
+      primary_sale_recipient: AddressZero,
+    });
+
+    // initialize the contract on the thirdweb sdk
+    const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+    // the contract's metadata
+    const metadata = await editionDrop.metadata.get();
+
+    console.log(
+      '✅ Successfully deployed editionDrop contract, address:',
+      editionDropAddress
+    );
+    console.log('🏷️ Edition Drop Metadata:', metadata);
+  } catch (err) {
+    console.error('❌ Failed to deploy editionDrop contract', err);
+    process.exit(1);
+  }
+})();
